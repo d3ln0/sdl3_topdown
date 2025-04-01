f7537d8c762ed7d11ebe69d0017178ae965ed913
d3ln0 <git@riedel-noah.de>
2025-04-01 13:11:16 +0200

    render player image

--- src/main.c
+++ src/main.c
@@ -1,13 +1,18 @@
 #include "SDL3/SDL_events.h"
 #include "SDL3/SDL_init.h"
+#include "SDL3/SDL_oldnames.h"
+#include "SDL3/SDL_rect.h"
 #include "SDL3/SDL_render.h"
+#include "SDL3/SDL_surface.h"
 #include "SDL3/SDL_video.h"
+#include "SDL3_image/SDL_image.h"
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 SDL_Window *window;
 SDL_Renderer *renderer;
+SDL_Texture *player_texture;
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     SDL_DestroyRenderer(renderer);
@@ -32,6 +37,12 @@ void update(){
 void render() {
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+    SDL_FRect sprite_portion = {17, 14, 15, 18};
+    SDL_FRect player_position = {250, 250, 15, 18};
+    SDL_SetTextureScaleMode(player_texture, SDL_SCALEMODE_LINEAR);
+    SDL_RenderTexture(renderer, player_texture, &sprite_portion, &player_position);
+
     SDL_RenderPresent(renderer);
 }
 
@@ -56,6 +67,9 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   if (!renderer) {
     SDL_Log("Error creating window: %s", SDL_GetError());
   }
+
+  const char path[] = "./assets/Top_Down_Adventure_Pack_v.1.0/Char_Sprites/char_spritesheet.png";
+  player_texture = IMG_LoadTexture(renderer, path);
 
   return SDL_APP_CONTINUE;
 }
